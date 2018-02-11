33d81f1dc545d39410cdadc9ebb2572102f72ed3
danival <danival.lucas@gmail.com>
2018-02-11 17:26:19 -0200

    config

--- server/config.js
+++ server/config.js
@@ -4,12 +4,12 @@ module.exports = {
     SPAWN_AREA: 3000, // 3000, // (raio em metros) área limite a partir da última posição conhecida do aparelho
     START_ENERGY: 10, //10, // (raio em metros) energia inicial do player
     MAX_ESTIMATED_ENERGY: 5000, //5000, // (metros) energia máxima estimada (os maiores players alcançarão uma energia próxima a essa)
-    DRIVING_MIN_VEL: 300, //30, // (km/h) velocidade térrea mínima (player próximo a MAX_ESTIMATED_ENERGY)
-    DRIVING_MAX_VEL: 3000, //300; // (km/h) velocidade térrea máxima (player com START_ENERGY)
+    DRIVING_MIN_VEL: 30, //30, // (km/h) velocidade térrea mínima (player próximo a MAX_ESTIMATED_ENERGY)
+    DRIVING_MAX_VEL: 300, //300; // (km/h) velocidade térrea máxima (player com START_ENERGY)
     DRIVING_MAX_DIST: 200000, // (metros) alcance de uma leg normal
     DIRECT_UNIT_COST: 10, //10, // (metros) custo fixo de energia para cada trecho direto
     DIRECT_MAX_DIST: 100, //100, // (x*player.energy) alcance de uma leg direta (porcentagem da energia do player)
-    DIRECT_VEL: 100000, //1000; // (km/h) fixa em 1.000 km/h (avião)
+    DIRECT_VEL: 1000, //1000; // (km/h) fixa em 1.000 km/h (avião)
     WAIT_AFTER_LEG: 0, //15000; // (miliseg) tempo de espera após comletar cada trecho
 
     // Food
